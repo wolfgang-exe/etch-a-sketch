1bce07e797c286f68fe31bcc8ac46f73a9d6b548
wolfgang-exe <wolfgang09999@gmail.com>
2024-12-25 01:08:00 -0700

    added ability to resize grid using slider

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+const BOARD_SIZE = 576;
+const styleSheets = document.styleSheets;
 const board = document.getElementById('board');
+const gridLabel = document.getElementById('grid-value');
+const gridSlider = document.getElementById('grid-size');
 
 let size = 16;
 createBoard(size);
 
 // create etch-a-sketch board
-function createBoard (size)
+function createBoard(size)
 {
+    // update box class to proper height and width
+    let newSize = `${BOARD_SIZE / size}` + "px";
+    for (const sheet of styleSheets)
+    {
+        for (const rule of sheet.cssRules)
+        {
+            if (rule.selectorText === '.box')
+            {
+                rule.style.height = `${newSize}`;
+                rule.style.width = `${newSize}`;
+            }
+        }
+    }    
+
+    // populate board with boxes
     for (let i = 1; i != size + 1; i++)
     {
         board.innerHTML += `<div id="row-${i}" class="row-container"></div>`
@@ -19,8 +38,9 @@ function createBoard (size)
         }
     }
 
+    // add mouseover even listeners
     let allBoxElements = document.querySelectorAll('.box');
-    addMouseHover(allBoxElements);
+    addMouseOver(allBoxElements);
 }
 
 // add border to board boxes if necessary
@@ -39,12 +59,27 @@ function addBorder(boxElement, i, k, size)
         boxElement.classList.add("left-border");
 }
 
+
+
 // add event listeners to each box
-function addMouseHover(allBoxElements)
+function addMouseOver(allBoxElements)
 {
     allBoxElements.forEach((box) => {
         box.addEventListener("mouseover", (box) => {
             box.target.style.backgroundColor = 'green';
         });
     });
+}
+
+// update grid label
+function updateGridLabel()
+{
+    gridLabel.innerHTML = `${gridSlider.value} X ${gridSlider.value}`;
+}
+
+// update board etch-a-sketch board
+function updateGridSize()
+{
+    board.innerHTML = ``;
+    createBoard(parseInt(gridSlider.value));
 }
